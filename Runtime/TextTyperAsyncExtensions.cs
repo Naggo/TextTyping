fb64ec62c54d6b8b482574227a9a3c852132dcb8
Naggo <naggo.plant@gmail.com>
2024-05-08 20:55:16 +0900

    Add ignoreStopping and other Updates

--- Runtime/TextTyperAsyncExtensions.cs
+++ Runtime/TextTyperAsyncExtensions.cs
@@ -10,12 +10,20 @@ using Cysharp.Threading.Tasks;
 namespace TextTyping {
     public static class TextTyperAsyncExtensions {
         public static async UniTask PlayAsync(this TextTyper typer, CancellationToken token = default) {
+            await typer.PlayAsync(false, token);
+        }
+
+        public static async UniTask PlayAsync(this TextTyper typer, bool ignoreStopping, CancellationToken token = default) {
             var typerToken = typer.GetCancellationTokenOnDestroy();
             using (var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(token, typerToken)) {
                 var linkedToken = linkedCts.Token;
                 typer.StartTyping();
                 linkedToken.Register(() => typer.StopTyping());
-                await UniTask.WaitUntil(() => typer.isCompleted, PlayerLoopTiming.Update, linkedToken);
+                if (ignoreStopping) {
+                    await UniTask.WaitUntil(() => typer.isCompleted, PlayerLoopTiming.Update, linkedToken);
+                } else {
+                    await UniTask.WaitUntil(() => typer.isCompleted || !typer.isUpdating, PlayerLoopTiming.Update, linkedToken);
+                }
             }
         }
     }
